8092cf02d2e7209ab77162af0fd737c23702f13a
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-12-07 16:06:58 +0100

    dev(feature): moving files

--- azure/steps/create_cluster/k8.ps1
+++ azure/steps/create_cluster/k8.ps1
@@ -2,4 +2,4 @@ Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
 Set-Location -Path "$HOME\repos\performance\arm-ttk\arm-ttk"
 Import-Module .\arm-ttk.psd1
 #This runs with pester
-Test-AzTemplate -TemplatePath C:\Users\gstarczewski\repos\performance\jmeter_azure_k8_boilerplate\pipelines\azure\arm\k8.json
+Test-AzTemplate -TemplatePath C:\Users\gstarczewski\repos\performance\jmeter_azure_k8_boilerplate\azure\steps\create_cluster\k8.json
