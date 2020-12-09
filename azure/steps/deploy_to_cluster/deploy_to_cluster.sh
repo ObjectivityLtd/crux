b8d784889643f7346c2ceb31696acf2c8881915c
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-12-09 16:18:48 +0100

    dev(feature): refactoring bash as per https://github.com/icy/bash-coding-style#quick-self-doc

--- azure/steps/deploy_to_cluster/deploy_to_cluster.sh
+++ azure/steps/deploy_to_cluster/deploy_to_cluster.sh
@@ -36,24 +36,18 @@ _wait_for_pods() {
 #display warning message if deployment is not correct e.g. more pods on nodes than allowed
 _display_deployment_correctness_status() {
   local _cluster_namespace=$1
-  printf "\nDeployment scheduled on: "
+  printf "\nDeployment scheduled on: \n"
   kubectl -n "$_cluster_namespace" get pods -o wide
   kubectl -n "$_cluster_namespace" get svc
-  local _rowsNumber="$( \
-    kubectl get -n "$_cluster_namespace" pods -o wide \
-    | awk '{print $7}' \
-    | wc -l)"
-
-  local _uniqueRowsNumber="$( \
-    kubectl get -n "$_cluster_namespace" pods -o wide \
-    | awk '{print $7}' \
-    | sort \
-    | uniq \
-    | wc -l)"
+  local _pods_nodes="$(kubectl get -n "$_cluster_namespace" pods -o wide | awk '{print $7}')"
+  local _pods_deployed_count="$(echo "$_pods_nodes" | wc -l)"
+  local _nodes_used_count="$(echo "$_pods_nodes" | sort | uniq | wc -l)"
   #more pods scheduled than nodes in cluster
-  if [ "$_rowsNumber" -gt "$_uniqueRowsNumber" ]; then
+  if [ "$_pods_deployed_count" -gt "$_nodes_used_count" ]; then
     echo "##[warning] There are more jmeter pods scheduled than nodes. You should not do that! Read why https://github.com/ObjectivityLtd/crux/wiki/FAQ"
     echo "##vso[task.complete result=SucceededWithIssues;]DONE"
+  else
+    printf "\n%s pods deployed on %s nodes" "$_pods_deployed_count" "$_nodes_used_count"
   fi
 }
 
