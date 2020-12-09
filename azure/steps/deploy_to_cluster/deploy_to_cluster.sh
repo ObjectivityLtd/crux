4985c348297757823105dd91b8ed31cd93e79050
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-12-09 15:39:11 +0100

    dev(feature): refactoring bash as per https://github.com/icy/bash-coding-style#quick-self-doc

--- azure/steps/deploy_to_cluster/deploy_to_cluster.sh
+++ azure/steps/deploy_to_cluster/deploy_to_cluster.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 #help functions
 
-wait_for_pod() {
-  local service_replicas="0/1"
-  local service_namespace=$1
-  local service=$2
-  local service_replicas_number=$3
-  local sleep_time_s=$4
-
-  status="Terminating"
-  printf "\nWait for service $service pods to be in Running status with interval $sleep_interval"
-  local elapsed=0
-  until [[ "$status" == "Running" ]]; do
-    sleep $sleep_time_s
-    status=$(kubectl -n $service_namespace get po | grep $service | awk '{print $3}' | sort | uniq) # this will display also old pods until they are gone
-    printf "\nService $service pods statuses: $(echo $status | xargs)"
+_wait_for_pod() { #public: wait for pods of a given type to be in Running state
+  local _service_replicas="0/1"
+  local _service_namespace=$1
+  local _service=$2
+  local _service_replicas_number=$3
+  local _sleep_time_s=$4
+  local _status="Terminating"
+  printf "\nWait for service %s pods to be in Running status with interval %s" "$_service" "$_sleep_time_s"
+  until [[ "$_status" == "Running" ]]; do
+    sleep "$_sleep_time_s"
+    _status=$(kubectl -n "$_service_namespace" get po | grep "$_service" | awk '{print $3}' | sort | uniq) # this will display also old pods until they are gone
+    printf "\nService %s pods statuses: %s " "$_service" "$(echo $_status | xargs)"
   done
 
 }
 
-wait_for_pods() {
-  local service_namespace=$1
-  local service_replicas_number=$2
-  local sleep_time_s=$3
+_wait_for_pods() {
+  local _service_namespace=$1
+  local _service_replicas_number=$2
+  local _sleep_time_s=$3
   shift 3
   IFS=' ' read -r -a services <<<"$@"
   for service in "${services[@]}"; do
-    wait_for_pod $service_namespace $service $service_replicas_number $sleep_time_s
+    _wait_for_pod $_service_namespace $service $_service_replicas_number $_sleep_time_s
   done
 
 }
 #display warning message if deployment is not correct e.g. more pods on nodes than allowed
-displayDeploymentCorrectnessStatus() {
+_display_deployment_correctness_status() {
   local cluster_namespace=$1
   echo ""
   echo "Deployment scheduled on: "
@@ -45,7 +43,6 @@ displayDeploymentCorrectnessStatus() {
     echo "##vso[task.complete result=SucceededWithIssues;]DONE"
   fi
 }
-
 
 #a bit too many steps but can support both ARM and k8 only
 deploy_to_cluster() {
@@ -98,9 +95,9 @@ deploy_to_cluster() {
   kubectl scale -n "$cluster_namespace" --replicas="$_crux_scale_up_replicas_master" -f "$rootPath/$jmeter_master_deploy_file"
   kubectl scale -n "$cluster_namespace" --replicas="$scale_up_replicas" -f "$rootPath/$jmeter_slaves_deploy_file"
 
-  wait_for_pods "$cluster_namespace" $_crux_scale_up_replicas_master $sleep_interval $service_master
-  wait_for_pods "$cluster_namespace" $scale_up_replicas $sleep_interval $service_slave
-  displayDeploymentCorrectnessStatus "$cluster_namespace"
+  _wait_for_pods "$cluster_namespace" $_crux_scale_up_replicas_master $sleep_interval $service_master
+  _wait_for_pods "$cluster_namespace" $scale_up_replicas $sleep_interval $service_slave
+  _display_deployment_correctness_status "$cluster_namespace"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
