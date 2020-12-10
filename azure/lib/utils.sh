9f5fdd04350495b5418120cd0f8d34041c0aa4e9
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-12-10 11:47:45 +0100

    dev(feature): refactoring bash as per https://github.com/icy/bash-coding-style#quick-self-doc

--- azure/lib/utils.sh
+++ azure/lib/utils.sh
@@ -1,21 +1,17 @@
 function log_warn(){
   local message=${1:-no message provided}
-  echo ""
   echo "##[warning]" "$message"
 }
 function log_info(){
   local message=${1:-no message provided}
-  echo ""
   echo "##[info]" "$message"
 }
 function log_error(){
   local message=${1:-no message provided}
-  echo ""
   echo "##[error]" "$message"
 }
 function mark_task_complete_with_issues(){
   local message=${1:-DONE}
-  echo ""
   echo "##vso[task.complete result=SucceededWithIssues;]$message"
 }
 
