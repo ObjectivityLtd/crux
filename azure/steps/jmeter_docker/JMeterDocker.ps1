582db2870923d92ecff4f8464a911ec997f284b4
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-12-17 10:04:00 +0100

    dev(feature): docker support for tests

--- azure/steps/jmeter_docker/JMeterDocker.ps1
+++ azure/steps/jmeter_docker/JMeterDocker.ps1
@@ -1,29 +1,37 @@
 param(
-  $RootPath="$PSScriptRoot",
-  $Image="gabrielstar/crux-master:0.0.1",
-  $ContainerName="crux",
-  $JMXPathOnAgent="${PSScriptRoot}/test_jmx/test_table_server.jmx",
-  $TestDataDirOnAgent = "${PSScriptRoot}/test_data",
-  $ContainerTestDataDir = "/test",
-  $UserArgs = "-Jthreads=10",
-  $FixedArgs = "-o $ContainerTestDataDir/report -f -l $ContainerTestDataDir/results.csv -e -Gsts=localhost -Gchromedriver=/usr/bin/chromedriver",
-  $SleepSeconds = 2
+    $RootPath = "$PSScriptRoot",
+    $Image = "gabrielstar/crux-master:0.0.1",
+    $ContainerName = "crux",
+    $JMXPathOnAgent = "${PSScriptRoot}/test_jmx/test_table_server.jmx",
+    $TestDataDirOnAgent = "${PSScriptRoot}/test_data",
+    $ContainerTestDataDir = "/test",
+    $UserArgs = "-Jthreads=10 -Jloops=10",
+    $FixedArgs = "-o $ContainerTestDataDir/report -f -l $ContainerTestDataDir/results.csv -e -Gsts=localhost -Gchromedriver=/usr/bin/chromedriver",
+    $SleepSeconds = 2,
+    $ArtifactsDirectory = "$PSScriptRoot/../../../kubernetes/tmp"
 )
 Import-Module $PSScriptRoot\JMeterDocker.psm1 -Force
 
-function Start-JMeterTests($RootPath, $Image, $ContainerName, $JMXPathOnAgent,$TestDataDirOnAgent,$ContainerTestDataDir,$UserArgs,$FixedArgs,$SleepSeconds) {
-  $testName = Split-Path $JMXPathOnAgent -leaf
-  Clear-Host
-  Copy-Item $JMXPathOnAgent $TestDataDirOnAgent
-  Stop-JMeterContainer -ContainerName $ContainerName
-  Start-JMeterContainer -RootPath $RootPath -Image $Image -ContainerName $ContainerName -TestDataDir $TestDataDirOnAgent -ContainerTestDataDir $ContainerTestDataDir
-  Start-SimpleTableServer -ContainerName $ContainerName -DataSetDirectory $ContainerTestDataDir -SleepSeconds $SleepSeconds
-  Show-TestDirectory -ContainerName $ContainerName -Directory $ContainerTestDataDir
-  Start-JmeterTest -ContainerName $ContainerName -JMXPath $ContainerTestDataDir/$testName -UserArgs $UserArgs -FixedArgs $FixedArgs
-  Show-TestDirectory -ContainerName $ContainerName -Directory $ContainerTestDataDir
-  Start-CommandInsideDocker $ContainerName "ls -alh $ContainerTestDataDir"
-  Stop-JMeterContainer -ContainerName $ContainerName
-
+function Copy-Artifacts($ArtifactsDirectory, $TestDataDirOnAgent)
+{
+    Copy-Item $TestDataDirOnAgent/report $ArtifactsDirectory
+    Copy-Item $TestDataDirOnAgent/jmeter.log $ArtifactsDirectory
+    Copy-Item $TestDataDirOnAgent/results.csv $ArtifactsDirectory
+    Copy-Item $TestDataDirOnAgent/errors.xml $ArtifactsDirectory
+}
+function Start-JMeterTests($RootPath, $Image, $ContainerName, $JMXPathOnAgent, $TestDataDirOnAgent, $ContainerTestDataDir, $UserArgs, $FixedArgs, $SleepSeconds)
+{
+    $testName = Split-Path $JMXPathOnAgent -leaf
+    Clear-Host
+    Copy-Item $JMXPathOnAgent $TestDataDirOnAgent
+    Stop-JMeterContainer -ContainerName $ContainerName
+    Start-JMeterContainer -RootPath $RootPath -Image $Image -ContainerName $ContainerName -TestDataDir $TestDataDirOnAgent -ContainerTestDataDir $ContainerTestDataDir
+    Start-SimpleTableServer -ContainerName $ContainerName -DataSetDirectory $ContainerTestDataDir -SleepSeconds $SleepSeconds
+    Show-TestDirectory -ContainerName $ContainerName -Directory $ContainerTestDataDir
+    Start-JmeterTest -ContainerName $ContainerName -JMXPath $ContainerTestDataDir/$testName -UserArgs $UserArgs -FixedArgs $FixedArgs
+    Show-TestDirectory -ContainerName $ContainerName -Directory $ContainerTestDataDir
+    Start-CommandInsideDocker $ContainerName "ls -alh $ContainerTestDataDir"
+    Stop-JMeterContainer -ContainerName $ContainerName
 }
 Start-JMeterTests -RootPath $RootPath `
                   -Image $Image `
@@ -34,3 +42,6 @@ Start-JMeterTests -RootPath $RootPath `
                   -UserArgs $UserArgs `
                   -FixedArgs $FixedArgs `
                   -SleepSeconds $SleepSeconds
+
+Copy-Artifacts -ArtifactsDirectory $ArtifactsDirectory `
+               -TestDirOnAgent $TestDataDirOnAgent
